142f9caf339b780d9da2fc3eb8978998c086254e
ADFdsadf32123 <mikon73261@alf5.com>
2026-05-30 21:53:11 +0800

    sda

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template> 
+  <div>
+    姓名：<input > <br>
+    月薪：<input > <br>
+    <button>提交</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+
+<style scoped>
+
+</style>
